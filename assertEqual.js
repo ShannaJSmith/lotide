79b0892c05733e39d151bfd5415a8cde6069f11c
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-14 00:33:29 +0000

    used assertion function to make colourful

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("same", "same");
+assertEqual("not", "the same");
+assertEqual(5, 5);
+assertEqual(1, 50);
+
