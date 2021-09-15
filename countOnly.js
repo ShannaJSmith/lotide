a582b9c56445da4c8cdc80ef0d9a63e13ad4cb90
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-15 22:05:29 +0000

    count only specific elements in an array

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+let pass = "✅✅✅Assertion Passed: ";
+let fail = "🛑🛑🛑Assertion Failed: ";
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${pass} ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {}
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+    }
+    console.log(item);
+  }
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }) 
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
