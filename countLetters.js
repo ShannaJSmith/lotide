b77942c5db5d0aa16750c455c00870487dd152f0
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-15 23:55:46 +0000

    print object from string

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+let pass = "✅✅✅Assertion Passed: ";
+let fail = "🛑🛑🛑Assertion Failed: ";
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${pass} ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+let objOfLetters = {};
+let newString = sentence.split(' ').join('')
+  for (i = 0; i < newString.length; i++) {
+   if (objOfLetters[newString[i]]) {
+     objOfLetters[newString[i]] += 1;
+   } else {
+     objOfLetters[newString[i]] = 1; 
+   }
+  } //console.log(objOfLetters);
+  return objOfLetters;
+} 
+const result1 = countLetters('lighthouse in the house');
+assertEqual(result1['l'], 1);
+assertEqual(result1['p'], undefined);
