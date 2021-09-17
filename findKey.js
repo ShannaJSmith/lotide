773adc8343a82dc01d501b17fb2a04709365371d
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-17 00:34:02 +0000

    used a function to find the first key a callback returns truthy for in an object

---
+++ findKey.js
@@ -0,0 +1,51 @@
+let pass = "✅✅✅Assertion Passed: ";
+let fail = "🛑🛑🛑Assertion Failed: ";
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${pass} ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail} ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    } 
+  } 
+}
+
+const actual = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // function(x) {if (x.start === 2) return true} else false}
+
+const actual2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 8)
+
+const actual3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars <= 3)
+
+
+assertEqual(actual, "noma")
+assertEqual(actual2, undefined)
+assertEqual(actual3, "Blue Hill")
+
+// 'key' = "blue hill", "noma", etc
