55ba23a24c11a2968f4884cd9f6fb1ba83a3fd79
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-19 21:44:15 +0000

    added arrays and cleaned format with eslint

--- eqObjects.js
+++ eqObjects.js
@@ -8,37 +8,51 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length){
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (const key of Object.keys(object1)) {
+    let isItAnArray = Array.isArray(object1)[key] && Array.isArray(object2[key]);
+    if (isItAnArray && eqArrays(object1[key], object2[key])) {
+      return true;
+    }
     if (object1[key] !== object2[key]) {
-
       return false;
     }
+    return true;
   }
-  return true;
-}
+};
 
 
-//Test Code for Primitives
+// //Test Code for Primitives
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // true
+console.log(eqObjects(ab, ba)); 
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // false
+console.log(eqObjects(ab, abc)); 
 
 console.log(assertEqual(eqObjects(ab, ba), true));
 console.log(assertEqual(eqObjects(ab, abc), false));
 
-// // Test Code for Arrays
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// eqObjects(cd, dc); // => true
-// assertEqual(eqObjects(cd, dc), true);
+// Test Code for Arrays
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc));
+console.log(assertEqual(eqObjects(cd, dc), true));
 
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// eqObjects(cd, cd2); // => false
-// assertEqual(eqObjects(cd, cd2), false);
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2));
+console.log(assertEqual(eqObjects(cd, cd2), false));
