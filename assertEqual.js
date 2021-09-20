167d72d3eda0f6080cde5474efebc7ea9d795959
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-20 05:21:21 +0000

    changed emoji

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 // let fail = "🛑🛑🛑Assertion Failed: ";
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🙌🏽 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`👀 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
