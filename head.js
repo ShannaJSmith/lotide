89724f7dc886fb3b4ea888624aedfbb6621201ef
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-20 05:52:56 +0000

    added missing code and test conditions

--- head.js
+++ head.js
@@ -9,9 +9,13 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(array) {
+  if (array === []) {
+    return undefined;
+  }
   return array[0];
 };
+
+head([10, 30, 2]);
+head([]);
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), "Hello");
